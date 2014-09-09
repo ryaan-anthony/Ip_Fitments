4bd15ca3484f566a73e66824ceb5eecc15efd954
Ryan Tulino <ryan@inverseparadox.net>
2014-09-09 17:11:34 -0400

    fix for support of < 1.9.x

--- app/code/community/Ip/Fitments/Helper/CatalogSearch.php
+++ app/code/community/Ip/Fitments/Helper/CatalogSearch.php
@@ -15,7 +15,7 @@ class Ip_Fitments_Helper_CatalogSearch extends Mage_CatalogSearch_Helper_Data
         if($params = Mage::getSingleton('customer/session')->getVehicleParams()){
             $_query = array(
                 '_query' => array(self::QUERY_VAR_NAME => $query),
-                '_secure' => $this->_getApp()->getFrontController()->getRequest()->isSecure()
+                '_secure' => Mage::app()->getFrontController()->getRequest()->isSecure()
             );
             $query_params = array_merge($_query, $params);
             return $this->_getUrl('vehiclesearch/result', $query_params);
